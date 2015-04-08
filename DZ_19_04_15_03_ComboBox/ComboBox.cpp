43cdd7d83de09c39b1588447461cf96a843e21b8
andrevyakin <andrevyakin@gmail.com>
2015-04-08 11:17:40 +0300

    Пустая форма

---
+++ DZ_19_04_15_03_ComboBox/ComboBox.cpp
@@ -0,0 +1,107 @@
+/*3.	На форме разместить список строк, однострочный редактор и кнопка «Найти».
+При нажатии на кнопку найти строку из редактора в списке строк.
+Если найти не удалось, то сообщить об этом.*/
+
+#include<Windows.h>
+#include<tchar.h>
+
+static TCHAR WindowsClass[] = L"win32app";
+static TCHAR Title[] = L"MyApp";
+HINSTANCE hinst;
+RECT desktop;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hinstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hinstance;
+	wcex.hIcon = LoadIcon(hinstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	/*wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);*/
+	wcex.hbrBackground = CreateSolidBrush(RGB(150, 70, 150));
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = WindowsClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL, L"Call faild!", L"MyApp", NULL);
+		return 1;
+	}
+
+	hinst = hinstance;
+	GetWindowRect(GetDesktopWindow(), &desktop);
+	HWND hWnd = CreateWindow(
+		WindowsClass,
+		Title,
+		WS_OVERLAPPEDWINDOW,
+		desktop.right / 4,
+		desktop.bottom / 6,
+		desktop.right / 2,
+		desktop.bottom / 2,
+		NULL,
+		NULL,
+		hinst,
+		NULL);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL, L"Create window faild!", L"MyApp", NULL);
+		return 1;
+	}
+	SetTimer(hWnd, 1, 100, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return(int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+
+	case WM_COMMAND:
+
+	case BN_CLICKED:
+
+		break;
+
+	case WM_CREATE:
+
+		break;
+
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
